093d1adf8dc7b4c0bf8b25e1ec6246421b8f57f8
Emmanuel Marcha <info@emmanuelmarcha.com>
2022-07-06 20:28:51 -0500

    Add CNAME to build (#2)

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function eleventyConfig(eleventyConfig) {
 	eleventyConfig.addWatchTarget('./sass/')
 	eleventyConfig.addPassthroughCopy('assets')
+	eleventyConfig.addPassthroughCopy('CNAME')
   
 	return {
 	  templateFormats: ['11ty.js', 'html']
 	}
-  }
+}
